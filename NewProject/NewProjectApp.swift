d2ea5fa1c8cd0ef7f992c48331951d7a5f04cf6f
MIA <mamatalievaindira1@gmail.com>
2023-04-10 18:00:42 +0600

    Initial Commit

---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by Indira on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
